d9530707b97c7675dd15925c75dbb0829bf1e096
RafiAbqor <rafi.abqor@gmail.com>
2024-10-21 17:35:22 +0700

    fix: import avatar

--- src/components/profile/my-profile.tsx
+++ src/components/profile/my-profile.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import { CurrentUserResponse } from "../../services/auth";
-import Avatar from "@/assets/avatar.svg";
+import Avatar from "../../assets/avatar.svg";
 
 interface MyProfileProps {
   user: CurrentUserResponse;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,11 +11,6 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
